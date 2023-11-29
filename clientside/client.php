d2e4454a4b97334ac92f7ca818f96486a0962784
Ilirjana Suka <118145049+IlirjanaSuka@users.noreply.github.com>
2023-11-29 14:08:06 +0100

    Update client.php

---
+++ clientside/client.php
@@ -0,0 +1,12 @@
+<?php
+// client config
+$serverHost = '127.0.0.1';
+$serverPort = 50000;
+
+// create socket
+$socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+
+if ($socket === false) {
+    echo "Failed to create socket: " . socket_strerror(socket_last_error()) . PHP_EOL;
+    exit;
+}
