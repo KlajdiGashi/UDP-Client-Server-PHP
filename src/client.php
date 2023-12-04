62334104abd6a2a0a11693a1ed16ada063fd49eb
KlajdiGashi <klajdi.gashi@student.uni-pr.edu>
2023-12-04 23:04:13 +0100

    formatted the code

--- src/client.php
+++ src/client.php
@@ -1,9 +1,7 @@
 <?php
-// client config
 $serverHost = '127.0.0.1';
 $serverPort = 50001;
 
-// create socket
 $socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
 
 if ($socket === false) {
@@ -11,14 +9,11 @@ if ($socket === false) {
     exit;
 }
 while (true) {
-    // read input from the console
     echo "Enter message (type 'exit' to quit): ";
     $input = rtrim(fgets(STDIN), "\r\n");
 
-    // send input to the server
     socket_sendto($socket, $input, strlen($input), 0, $serverHost, $serverPort);
 
-    // receive and display the response from the server
     $bytesReceived = socket_recvfrom($socket, $response, 1024, 0, $serverHost, $serverPort);
 
     if ($bytesReceived === false) {
@@ -32,6 +27,5 @@ while (true) {
     }
 }
 
-// close the socket
 socket_close($socket);
 ?>
--- src/server.php
+++ src/server.php
@@ -1,32 +1,26 @@
 <?php
-// Server config
+
 $serverHost = '127.0.0.1';
 $serverPort = 50001;
-// Password
+
 $adminPassword = 'passwordis123';
 
-// Create socket
 $socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
 socket_bind($socket, $serverHost, $serverPort);
 
 echo "UDP Server listening on $serverHost:$serverPort\n";
 
-// Associative array to store client-specific data
 $clients = [];
 $loggedInUsers = [];  // Array to track successfully logged-in users
 while (true) {
-    // Leximi i te dhenave nga klienti
     socket_recvfrom($socket, $buffer, 1024, 0, $clientAddress, $clientPort);
     echo "Received from $clientAddress:$clientPort: $buffer\n";
 
-    // processimi i te dhenave
     $response = processRequest($buffer, $adminPassword, $clients, $loggedInUsers);
 
-    // dergimi i nje response per klientin
     if ($clientPort !== null) {
         socket_sendto($socket, $response, strlen($response), 0, $clientAddress, $clientPort);
     } else {
-        // Handle the case when $clientPort is null (e.g., log an error, display a message, etc.)
         echo "Error: Client port is null.\n";
     }
 
@@ -36,30 +30,24 @@ while (true) {
 }
 socket_close($socket);
 
-// funskioni kryesor per procesimin e kerkesave te klientit
 function processRequest($request, $adminPassword, &$clientData, &$loggedInUsers)
 {
     $response = "Invalid command";
     echo "Raw request: $request\n";
 
-    // ndarja e kerkesave ne 2 pjese 
     $parts = explode(" ", $request);
 
-    // Marrja e komandes dhe password
     $command = isset($parts[0]) ? $parts[0] : '';
     $password = isset($parts[1]) ? $parts[1] : '';
 
-    // Marrja e pjeses tjeter te inputit
     $content = implode(" ", array_slice($parts, 2));
 
     if (!empty($command) && !empty($password)) {
-        // Use switch case for command processing
-
-        // perdorimi i switch case per kontrollimin e komandave
         switch ($command) {
             case '/password':
                 // kontrollimi i password
                 if ($password === $adminPassword) {
+                    $loggedInUsers[] = $clientData['address'];
                     $response = "Administrator login successful";
                 } else {
                     $response = "Administrator login failed";
@@ -70,14 +58,14 @@ function processRequest($request, $adminPassword, &$clientData, &$loggedInUsers)
                 if (isset($parts[2]) && isset($parts[3])) {
                     if (in_array($clientData['address'], $loggedInUsers)) {
                         $fileName = 'output.txt';
-                        $fileContent = $content;  // Using the extracted content variable
+                        $fileContent = $content;  
 
-                        // Shkrimi ne file
+                        
                         if (file_put_contents($fileName, $fileContent, LOCK_EX) !== false) {
-                            // e bon lock release qe me u mbyll file dhe me marr inputin e sakt
+                            
                             fclose(fopen($fileName, 'a'));
                             $response = "File '$fileName' written successfully.";
-                            // Ruajtja e flie-it ne daten e klientit
+                            
                             $clientData['writtenContent'] = $fileContent;
                         } else {
                             $response = "Failed to write to file '$fileName'.";
@@ -91,7 +79,7 @@ function processRequest($request, $adminPassword, &$clientData, &$loggedInUsers)
                 break;
 
             case '/read':
-                // lexon kontekstin brenda file-it
+                
                 $fileName = 'output.txt';
 
                 if (file_exists($fileName)) {
@@ -103,11 +91,11 @@ function processRequest($request, $adminPassword, &$clientData, &$loggedInUsers)
                 break;
 
             case '/execute':
-                // Kontrollon nese perdoruesi eshte administrator
+                
                 if (in_array($clientData['address'], $loggedInUsers) && $password === $adminPassword) {
                     $fileName = 'output.txt';
 
-                    // Fshin file-n ekzistues
+                    
                     if (file_exists($fileName)) {
                         unlink($fileName);
                         $response = "Execution privileges granted for administrators. File '$fileName' deleted.";
@@ -128,7 +116,7 @@ function processRequest($request, $adminPassword, &$clientData, &$loggedInUsers)
                     break;
 
             case '/help':
-                // Provide help information for the /help command
+                
                 $response = "Available commands:\n";
                 $response .= "/password <password> - Log in as administrator\n";
                 $response .= "/write <password> <content> - Write content to file\n";
